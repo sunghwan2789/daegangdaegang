a7671f9570775768a2d3078647a8c13ad781cb6c
Sunghwan Bang <sunghwan2789@gmail.com>
2019-10-18 14:39:43 +0900

    Update settings.py

--- daegang/settings.py
+++ daegang/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'hs7q1^*9dlflznyo)z4dblhw8qcph#
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
